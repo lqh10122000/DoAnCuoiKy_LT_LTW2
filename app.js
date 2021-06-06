722966dd77bccd723d95bacef9f5e96d1566e216
Nguyentanduy <duynguyen251000@gmail.com>
2021-06-06 23:33:50 +0700

    movie trailer

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const expressLayouts = require("express-ejs-layouts");
 const cookieSession = require("cookie-session");
-
 //router
 const db = require("./models/db");
 const userRouter = require("./routers/user");
