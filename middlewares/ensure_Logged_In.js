069f2f96b6af97a3dd3792ceb3c986755766eee8
duy2000-20 <nduy94372@gmail.com>
2021-06-11 22:59:26 +0700

    Change

--- middlewares/ensure_Logged_In.js
+++ middlewares/ensure_Logged_In.js
@@ -1,5 +1,5 @@
-module.exports = function ensureLoggedIn (req, res, next){
-    if(!req.currentUser){
+module.exports = function ensureLoggedIn(req, res, next) {
+    if (!req.currentUser) {
         res.redirect('/auth/login');
     } else {
         next();
