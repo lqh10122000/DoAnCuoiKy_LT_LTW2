e51db7ecc8e74caa285956cea7a57b5e9d6a52f7
dtbao23 <dtbao23@gmail.com>
2021-07-11 10:50:55 +0700

    fix db

--- models/db.js
+++ models/db.js
@@ -4,11 +4,10 @@ module.exports = new Sequelize(
   process.env.DATABASE_URL ||
     `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`,
   {
-    dialect: "postgres",
+    dialect: 'postgres',
     dialectOptions: {
         ssl: {
             rejectUnauthorized: false,
         }
     } 
-  }
-);
+  });
