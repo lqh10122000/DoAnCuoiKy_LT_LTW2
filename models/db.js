da389b35fa2c6a78cd40b533d7d56ab61f6deba6
dtbao23 <dtbao23@gmail.com>
2021-07-11 11:12:38 +0700

    1.0

--- models/db.js
+++ models/db.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 module.exports = new Sequelize(
-  process.env.DATABASE_URL /* ||
-    `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}` */,
+  process.env.DATABASE_URL ||
+    `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}` ,
   {
     dialect: 'postgres',
     dialectOptions: {
