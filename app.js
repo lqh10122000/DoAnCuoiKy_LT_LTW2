709a85119263d91b730b609691cd652f07b132a5
dtbao23 <dtbao23@gmail.com>
2021-07-15 16:19:50 +0700

    fix

--- app.js
+++ app.js
@@ -22,7 +22,9 @@ const app = express();
 app.set("views", "views");
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({extended: true})); 
+//app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressLayouts);
 app.use(express.static("public"));
 
